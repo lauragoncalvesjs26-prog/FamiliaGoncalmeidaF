22a959a7fadd4dd5a863c62c65d362ca9113a813
Laura Alves <laura.xuxu.alves@gmail.com>
2026-08-11 19:18:25 -0300

    quarto envio

--- src/api.jsx
+++ src/api.jsx
@@ -3,5 +3,5 @@ const isLocalhost =
     window.location.hostname === "127.0.0.1";
 
 export const API_URL = isLocalhost
-    ? "http://localhost:8888"
-    : "backend online";
+    ? "http://localhost:8888" //link local
+    : "https://familiagoncalmeidab.onrender.com"; // link online
